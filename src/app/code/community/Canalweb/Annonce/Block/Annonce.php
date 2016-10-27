95d9e36f6823e8ebf9517bf363babcab99720db1
deb <deborah@canal-web.fr>
2016-10-27 17:25:10 +0200

    add conf and launch slider

--- src/app/code/community/Canalweb/Annonce/Block/Annonce.php
+++ src/app/code/community/Canalweb/Annonce/Block/Annonce.php
@@ -15,4 +15,18 @@ class Canalweb_Annonce_Block_Annonce extends Mage_Core_Block_Template {
         }
         return $messages;
     }
+
+    /**
+     *  Get configuration parameters
+     */
+    public function getConf() {
+        $activation = Mage::getStoreConfig('annonce/configuration/activation');
+        $autoplay = Mage::getStoreConfig('annonce/configuration/autoplay');
+        $speed = Mage::getStoreConfig('annonce/configuration/speed');
+        $conf = array();
+        $conf['activation'] = $activation ? $activation : 0;
+        $conf['autoplay'] = $autoplay ? $autoplay : 0;
+        $conf['speed'] = $speed ? $speed : 5000;
+        return $conf;
+    }
 }
